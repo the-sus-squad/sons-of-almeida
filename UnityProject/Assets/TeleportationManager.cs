f31bb9124d6d0e84c21c3f85eaf542cb45bcb3d5
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-03-26 13:04:25 +0100

    OnTeleport handler created

--- UnityProject/Assets/TeleportationManager.cs
+++ UnityProject/Assets/TeleportationManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.CoreModule;
+// using UnityEngine.CoreModule;
 
 public class TeleportationManager : MonoBehaviour
 {
@@ -11,40 +11,31 @@ public class TeleportationManager : MonoBehaviour
     [SerializeField] private XRRayInteractor rayInteractor;
     private InputAction _thumbstick;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Custom start");
-        // rayInteractor.enabled = false;
-        actionAsset = ScriptableOject.CreateInstance<InputActionAsset>();
+        // Used to movement instead of teleport...?
+        // var activate = actionAsset.FindActionMap("XRI LeftHand Locomotion").FindAction("Teleport Mode Activate");
+        // activate.Enable();
 
-        var activate = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Activate");
+        // // .performed = list of methods when the told action is made.
+        // activate.performed += OnTeleportActivate;
+
+        var activate = actionAsset.FindActionMap("XRI LeftHand Locomotion").FindAction("Teleport Mode Cancel");
         activate.Enable();
-        // .performed = list of methods when the told action is made.
-        activate.performed += OnTeleportActivate;
+        activate.performed += OnTeleport;
 
-        var cancel = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Activate");
-        cancel.Enable();
-        activate.performed += OnTeleportCancel;
-
-
-        _thumbstick = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Activate");
-        _thumbstick.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void OnTeleportActivate(InputAction.CallbackContext context) {
-        Debug.Log("Sussy Teleport");
-        // rayInteractor.enabled = true;
-    }
-
-    void OnTeleportCancel(InputAction.CallbackContext context) {
-        // rayInteractor.enabled = false;
 
     }
+
+    void OnTeleport(InputAction.CallbackContext context) {
+        Debug.Log("Teleportus");
+    }
+
 }
