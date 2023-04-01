11dd15dda37838687833b7e2ebdbc9c089a4df93
Nuno Veloso <a2019130833@isec.pt>
2023-04-01 20:57:45 +0100

    3 - Continuous movement towards camera
    - Changed from periodic to continuous movement towards camera, with editable speed.
    - Commented old version in FollowCamera.cs script.

--- UnityProject/Assets/FollowCamera.cs
+++ UnityProject/Assets/FollowCamera.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
 
-    public GameObject camera;
+    [SerializeField] private GameObject camera;
+    private float smoothTime = 0.7F;
+    private Vector3 velocity = Vector3.zero;
+
+    // Periodic movement every x seconds towards camera 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("followCamera", 2f, 5f);
-    }
+    //void Start()
+    //{
+    //    InvokeRepeating("followCamera", 3f, 3f);
+    //}
 
-    void followCamera()
-    {
-        transform.rotation = camera.transform.rotation;
-        transform.position = camera.transform.position;
-        //Debug.Log(transform.forward);
-        transform.position += transform.forward * 2;
-    }
+    //void followCamera()
+    //{
+    //    transform.rotation = camera.transform.rotation;
+    //    transform.position = camera.transform.position;
+    //    transform.position += transform.forward * 2;
+    //}
+
+
+    // Continuous movement towards camera
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = camera.transform.rotation;
+        transform.position = Vector3.SmoothDamp(transform.position, camera.transform.position + (camera.transform.forward * 2), ref velocity, smoothTime);
     }
 }
