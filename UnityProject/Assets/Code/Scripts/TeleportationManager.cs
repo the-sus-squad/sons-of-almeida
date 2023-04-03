75fa3f8854f48da7107918441b43f5b76cf953b4
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-04-03 14:22:40 +0100

    On teleport release trigger found

--- UnityProject/Assets/Code/Scripts/TeleportationManager.cs
+++ UnityProject/Assets/Code/Scripts/TeleportationManager.cs
@@ -17,7 +17,7 @@ public class TeleportationManager : MonoBehaviour
     [SerializeField] private Image fadeImage;
     [SerializeField] private Animator fadeAnimator;
     [SerializeField] private GameObject fade;
-// 
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +29,10 @@ public class TeleportationManager : MonoBehaviour
 
         fade.SetActive(true);
 
+        var release = actionAsset.FindActionMap("XRI LeftHand Locomotion").FindAction("Teleport Mode Activate");
+        release.Enable();
+        release.performed += OnTeleportRelease;
+
     }
 
     // Update is called once per frame
@@ -39,10 +43,17 @@ public class TeleportationManager : MonoBehaviour
 
     void OnTeleport(InputAction.CallbackContext context) {
         if (!rayInteractor.enabled) return;
-        Debug.Log("Teleportus"); 
+        Debug.Log("Teleportus");
         Debug.Log(context);
-        fadeAnimator.Play("FadeIn");
         // rayInteractor.enabled = false;
+    }
+
+    void OnTeleportRelease(InputAction.CallbackContext context) {
+        Debug.Log("Teleport release");
+    }
+
+    public void OnSussy() {
+        Debug.Log("Sus");
     }
 
 }
