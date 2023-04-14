3f539c0bdb9b03ea7d05f0ae702e907d0dd84ed9
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2023-04-14 17:33:47 +0100

    Fix hands physics making hands uncontrollable

---
+++ UnityProject/Assets/Code/Scripts/HandPhysics.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPhysics : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Rigidbody rb;
+    private Collider[] handColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = true;
+        }
+    }
+
+    public void EnableHandColliderDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
+    }
+
+    public void DisableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+
+        rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad) / Time.fixedDeltaTime;
+    }
+}
