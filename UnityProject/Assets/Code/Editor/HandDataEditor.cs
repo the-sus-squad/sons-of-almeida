118143c427bef6c90481ffddb7450986b66ff0cb
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2023-04-23 16:48:24 +0100

    Activate rotation handle when a joint is selected

--- UnityProject/Assets/Code/Editor/HandDataEditor.cs
+++ UnityProject/Assets/Code/Editor/HandDataEditor.cs
@@ -6,6 +6,9 @@ using UnityEngine.UIElements;
 public class HandDataEditor : Editor
 {
     private HandData handData = null;
+    private Transform activeJoint = null;
+    private Color defaultColor = new Color(1, 1, 1);
+    private Color activeColor = new Color(1, 0, 0);
 
     private void OnEnable()
     {
@@ -15,16 +18,43 @@ public class HandDataEditor : Editor
     private void OnSceneGUI()
     {
         DrawJointButtons();
+        if (activeJoint)
+        {
+            DrawJointRotationGizmo();
+        }
     }
 
     private void DrawJointButtons()
     {
         foreach (Transform joint in handData.fingerBones)
         {
+            Handles.color = defaultColor;
+            if (joint == activeJoint)
+            {
+                Handles.color = activeColor;
+            }
+
             if (Handles.Button(joint.position, joint.rotation, 0.01f, 0.005f, Handles.SphereHandleCap))
             {
-
+                // if the same joint is selected then turn off the rotation gizmo
+                activeJoint = IsSelected(joint) ? null : joint;
             }
+        }
+    }
+
+    private bool IsSelected(Transform joint)
+    {
+        return joint == activeJoint;
+    }
+
+    private void DrawJointRotationGizmo()
+    {
+        Quaternion newRotation = Handles.RotationHandle(activeJoint.rotation, activeJoint.position);
+
+        if (newRotation != activeJoint.rotation)
+        {
+            Undo.RecordObject(activeJoint, "Rotated joint");
+            activeJoint.rotation = newRotation;
         }
     }
 }
