1d46cb28d9b0bca16476db5adbadd1b75ed8ba8d
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-04-24 19:23:07 +0100

    Basic field of view

---
+++ UnityProject/Assets/Code/Scripts/EnemyAI/EnemyNavigation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public GameObject target;
+    
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     agent.SetDestination(target.transform.position);
+    // }
+
+}
---
+++ UnityProject/Assets/Code/Scripts/EnemyAI/FieldOfView.cs
@@ -0,0 +1,73 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Vector3 = UnityEngine.Vector3;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius;
+
+    [Range(0, 360)]
+    public float angle;
+    public GameObject player;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FOVRoutine());
+    }
+
+    // Not call every frame
+    private IEnumerator FOVRoutine() {
+        float delay = 0.2f;
+        WaitForSeconds wait = new WaitForSeconds(delay);
+
+        while (true) {
+            // wtf
+            yield return wait;
+            Check();
+            
+        }
+    }
+
+    private void Check() {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0) {
+            Transform target = rangeChecks[0].transform; // Cursed hardcoded index, however it is because there is just one player
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2) {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)) {
+                    canSeePlayer = true;
+                }
+                else {
+                    canSeePlayer = false;
+                }
+            }
+            else {
+                // Player not here
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer) {
+            canSeePlayer = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
