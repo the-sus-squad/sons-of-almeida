f2ee407641ef41a0200518a01d1a8f5a82d4ee71
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2023-05-13 18:31:31 +0100

    Add door script to open and close the door

--- UnityProject/Assets/Code/Scripts/Door.cs
+++ UnityProject/Assets/Code/Scripts/Door.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] private Transform doorHandle;
+
+    private HingeJoint doorHandleHinge;
+    private Rigidbody doorRB;
+    private HingeJoint doorHinge;
+
+    private float threshold = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorHandleHinge = doorHandle.GetComponent<HingeJoint>();
+        doorRB = GetComponent<Rigidbody>();
+        doorHinge = GetComponent<HingeJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (doorHinge.angle <= doorHinge.limits.min + threshold)
+        {
+            doorRB.freezeRotation = true;
+        }
+
+        if (doorHandleHinge.angle >= doorHandleHinge.limits.max - threshold)
+        {
+            doorRB.freezeRotation = false;
+        }
     }
 }
