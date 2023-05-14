afaf88c62d18d38cabd23a128117cb04e2879508
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2023-05-14 16:47:51 +0100

    Add fade out animation when exiting game

--- UnityProject/Assets/Code/Scripts/MainMenuManager.cs
+++ UnityProject/Assets/Code/Scripts/MainMenuManager.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private HingeJoint door;
+    [SerializeField] private GameObject fadeOut;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private HingeJoint door;
+    [SerializeField] private float doorExitAngle = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (door.angle > 10.0f)
+        if (door.angle > doorExitAngle)
         {
-            Debug.Log("Exit Game");
+            ExitGame();
         }
+    }
+
+    private void ExitGame()
+    {
+        fadeOut.SetActive(true);
+        fadeOut.GetComponent<Animator>().Play("FadeOut");
+
+        Debug.Log("Exit Game");
     }
 }
