bdbbdb7d963de3b5e84a39d962ee72eda001c4f6
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-05-18 21:20:13 +0100

    Collision detector + base teleport to camera view

--- UnityProject/Assets/Code/Scripts/EnemyAI/EnemyNavigation.cs
+++ UnityProject/Assets/Code/Scripts/EnemyAI/EnemyNavigation.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,22 +19,30 @@ public class EnemyNavigation : MonoBehaviour
     public float searchRadius;
     public Vector3 destination;
 
+    // public UnityEvent OnTargetCaptured;
 
     private bool seenTarget = false;
+    private bool isCapturingPlayer = false;
+    private bool isOnCamera = false;
     
     // Update is called once per frame
     void Update()
     {
-        if (seenTarget) {
-            agent.SetDestination(target.transform.position);
+        if (isCapturingPlayer) {
         }
         else {
-            // If the agent has reached the destination, or if there is no destination, find a new one.
-            if ((destination == Vector3.zero || transform.position.x == destination.x && transform.position.z == destination.z) 
-            && RandomPoint(transform.position, searchRadius, out destination)) {
-                agent.SetDestination(destination);
+            if (seenTarget) {
+                agent.SetDestination(target.transform.position);
+            }
+            else {
+                // If the agent has reached the destination, or if there is no destination, find a new one.
+                if ((destination == Vector3.zero || transform.position.x == destination.x && transform.position.z == destination.z) 
+                && RandomPoint(transform.position, searchRadius, out destination)) {
+                    agent.SetDestination(destination);
+                }
             }
         }
+        
     }
 
     public void seeTarget(bool value) {
@@ -70,6 +79,51 @@ public class EnemyNavigation : MonoBehaviour
             agent.SetDestination(destination);
             seenTarget = false;
         }
+    }
+
+    private void OnTriggerEnter(Collider t) {
+        Debug.Log("Triggered collision with " + t.gameObject.name);
+        if (t.gameObject == target) {
+            CapturePlayer();
+        }
+    }
+
+    private void CapturePlayer() {
+        Debug.Log("Captured player");
+        isCapturingPlayer = true;
+
+        agent.isStopped = true;
+        // OnTargetCaptured.Invoke();
+        // Change destination to targets's front
+        if (isOnCamera) {
+            // agent.SetDestination(target.transform.position + target.transform.forward * 2);
+            Debug.Log("Is on camera");
+        }
+        else {
+            Debug.Log("Is not on camera");
+            transform.position = target.transform.position + target.transform.forward * 2;
+        }
+
+    }
+
+    void OnBecameInvisible() {
+        // #if UNITY_EDITOR
+        // if (Camera.current.name == "SceneCamera") 
+        //     return;
+        // #endif
+
+        Debug.Log("Became invisible");
+        isOnCamera = false;
+    }
+
+    void OnBecameVisible() {
+        // #if UNITY_EDITOR
+        // if (Camera.current.name == "SceneCamera") 
+        //     return;
+        // #endif
+
+        Debug.Log("Became visible");
+        isOnCamera = true;
     }
 
 }
