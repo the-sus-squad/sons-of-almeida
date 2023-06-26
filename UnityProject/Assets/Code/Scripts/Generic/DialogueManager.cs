cac134b146149b839cfcc4f553833661381fea3e
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-06-26 15:03:03 +0100

    End of Church dialogue + Enemy spawning event

--- UnityProject/Assets/Code/Scripts/Generic/DialogueManager.cs
+++ UnityProject/Assets/Code/Scripts/Generic/DialogueManager.cs
@@ -6,21 +6,31 @@ public class DialogueManager : MonoBehaviour
 {
 
     public SubtitleInteraction subtitle;
+    public EnemyRevealer enemyRevealer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SayHello() {
         Debug.Log("Hello Prologers!");
         subtitle.ShowMessage("Hello Prologers!");
+    }
+
+    public void EndOfChurch() {
+        Debug.Log("End of church");
+        StartCoroutine(EndOfChurchRoutine());
+    }
+
+    IEnumerator EndOfChurchRoutine() {
+        Debug.Log("Routine");
+        subtitle.ShowMessage("Oh no, the exit is locked?");
+        yield return new WaitForSeconds(6f);
+
+        subtitle.ShowMessage("I need to find a way out of here!");
+        yield return new WaitForSeconds(6f);
+
+        subtitle.ShowMessage("What are those noises?");
+        yield return new WaitForSeconds(1f);
+        enemyRevealer.ActivateEnemies();
+        yield return new WaitForSeconds(6f);
+        subtitle.ShowMessage("Oh no...");
     }
 }
