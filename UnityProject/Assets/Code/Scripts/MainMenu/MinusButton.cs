38f0714c857fc71b746c84b09b84344daf9a1cbc
Daniel Francisco <deny.jo@hotmail.com>
2023-06-26 23:36:19 +0100

    Button change color when pressed

--- UnityProject/Assets/Code/Scripts/MainMenu/MinusButton.cs
+++ UnityProject/Assets/Code/Scripts/MainMenu/MinusButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class MinusButton : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class MinusButton : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        GetComponentInChildren<Image>().color = new Color(0.5283019f, 0.5283019f, 0.5283019f);
         HitMinusButton.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponentInChildren<Image>().color = new Color(1.0f, 1.0f, 1.0f);
     }
 }
